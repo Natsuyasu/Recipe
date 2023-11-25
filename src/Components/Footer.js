9b2970b463f0e5eac4e8755fd28a6b41a0200495
Natsuyasu <xiaan706@gmail.com>
2023-11-25 16:55:10 +0800

    update footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,7 +9,7 @@ function Footer() {
   return (
     <div className="text-sm text-zinc-500 mb-6 pt-10 pb-4 mb:pt-4 pb-4">
       <div className="flex justify-center">
-        &copy; 2023 <a href="https://github.com/Natsuyasu">&nbsp; {element} Vivia</a> <a href="https://github.com/Ex10si0n">&nbsp; {element} Ex10si0n</a>.
+        &copy; 2023 <a href="https://github.com/Natsuyasu">&nbsp; {element} Vivia</a> <a href="https://github.com/Ex10si0n">&nbsp; {element} Ex10si0n</a><a href="https://github.com/dodaydream">&nbsp; {element} Stanley</a>.
       </div>
       <div className="flex justify-center">Version: 0.2</div>
       <div className="flex justify-center"> &#127836; 好好吃饭~</div>
